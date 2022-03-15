716ee9958f47ed98290dbe4bb1d6106a8d64f967
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2022-03-15 13:20:54 -0400

    Add more constraints

--- pyroed/constraints.py
+++ pyroed/constraints.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Dict, Optional
 
 import torch
 
@@ -7,48 +7,20 @@ from .typing import Schema
 
 
 class Constraint(ABC):
+    """
+    Abstract base class for constraints
+    """
+
     @abstractmethod
     def __call__(self, schema: Schema, choices: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError
 
 
-class AllDifferent(Constraint):
-    def __init__(self, *names):
-        super().__init__()
-        self.names = names
-        self.schema = None
-        self.name_to_int = None
-        self.standardize = None
-
-    def __str__(self):
-        return "AllDifferent({})".format(", ".join(map(repr, self.names)))
-
-    def __call__(self, schema: Schema, choices: torch.Tensor) -> torch.Tensor:
-        # Lazily build tensor indices.
-        if schema is not self.schema:
-            self.schema = schema
-            self.name_to_int = {name: i for i, name in enumerate(schema)}
-            standard = list(set().union(*(schema[n] for n in self.names)))
-            standard.sort(key=lambda x: (0, "") if x is None else (1, x))
-            self.standardize = choices.new_empty(
-                len(self.names), max(len(schema[n]) for n in self.names)
-            )
-            for i, name in enumerate(self.names):
-                for j, value in enumerate(schema[name]):
-                    self.standardize[i, j] = standard.index(value)
-
-        # Compute the constraint.
-        ps = [self.name_to_int[name] for name in self.names]
-        ok = torch.tensor(True)
-        for i, p1 in enumerate(ps):
-            c1 = self.standardize[i][choices[..., p1]]
-            for j, p2 in enumerate(ps[:i]):
-                c2 = self.standardize[j][choices[..., p2]]
-                ok = ok & (c1 != c2)
-        return ok
-
-
 class TakesValue(Constraint):
+    """
+    Constrains a site to take a fixed value.
+    """
+
     def __init__(self, name: str, value: Optional[str]):
         super().__init__()
         self.name = name
@@ -70,6 +42,160 @@ class TakesValue(Constraint):
         raise ValueError(
             f"In constraint {self}: {repr(self.value)} not found in schema"
         )
+
+
+class TakesValues(Constraint):
+    """
+    Constrains a site to take one of a set of values.
+    """
+
+    def __init__(self, name: str, *values: Optional[str]):
+        super().__init__()
+        self.name = name
+        self.values = values
+        self.schema: Optional[Schema] = None
+        self.index: Optional[int] = None
+        self.mask: Optional[torch.Tensor] = None
+
+    def __str__(self):
+        args = ", ".join(map(repr, (self.name,) + self.values))
+        return f"TakesValues({args})"
+
+    def __call__(self, schema: Schema, choices: torch.Tensor) -> torch.Tensor:
+        # Lazily build tensor indices.
+        if schema is not self.schema:
+            assert self.name in schema
+            for index, (name, values) in enumerate(schema.items()):
+                if name == self.name:
+                    self.schema = schema
+                    self.index = index
+                    self.mask = torch.zeros(len(values), dtype=torch.bool)
+                    assert set(self.values).issubset(values)
+                    for i, value in enumerate(values):
+                        if value in self.values:
+                            self.mask[i] = True
+                    break
+        assert self.mask is not None
+        assert self.index is not None
+
+        # Compute the constraint.
+        return self.mask[choices[..., self.index]]
+
+
+class AllDifferent(Constraint):
+    """
+    Constrains a set of sites to all have distinct values.
+    """
+
+    def __init__(self, *names: str):
+        super().__init__()
+        self.names = names
+        self.schema: Optional[Schema] = None
+        self.name_to_int: Optional[Dict[str, int]] = None
+        self.standardize: Optional[torch.Tensor] = None
+
+    def __str__(self):
+        return "AllDifferent({})".format(", ".join(map(repr, self.names)))
+
+    def __call__(self, schema: Schema, choices: torch.Tensor) -> torch.Tensor:
+        # Lazily build tensor indices.
+        if schema is not self.schema:
+            self.schema = schema
+            self.name_to_int = {name: i for i, name in enumerate(schema)}
+            standard = list(set().union(*(schema[n] for n in self.names)))
+            standard.sort(key=lambda x: (0, "") if x is None else (1, x))
+            self.standardize = choices.new_empty(
+                len(self.names), max(len(schema[n]) for n in self.names)
+            )
+            for i, name in enumerate(self.names):
+                for j, value in enumerate(schema[name]):
+                    self.standardize[i, j] = standard.index(value)
+        assert self.name_to_int is not None
+        assert self.standardize is not None
+
+        # Compute the constraint.
+        ps = [self.name_to_int[name] for name in self.names]
+        ok = torch.tensor(True)
+        for i, p1 in enumerate(ps):
+            c1 = self.standardize[i][choices[..., p1]]
+            for j, p2 in enumerate(ps[:i]):
+                c2 = self.standardize[j][choices[..., p2]]
+                ok = ok & (c1 != c2)
+        return ok
+
+
+class Not(Constraint):
+    """
+    Negates a constraints.
+    """
+
+    def __init__(self, arg: Constraint):
+        super().__init__()
+        self.arg = arg
+
+    def __str__(self):
+        return f"Not({self.arg})"
+
+    def __call__(self, schema: Schema, choices: torch.Tensor) -> torch.Tensor:
+        arg = self.arg(schema, choices)
+        return ~arg
+
+
+class And(Constraint):
+    """
+    Conjoins two constraints.
+    """
+
+    def __init__(self, lhs: Constraint, rhs: Constraint):
+        super().__init__()
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def __str__(self):
+        return f"And({self.lhs}, {self.rhs})"
+
+    def __call__(self, schema: Schema, choices: torch.Tensor) -> torch.Tensor:
+        lhs = self.lhs(schema, choices)
+        rhs = self.rhs(schema, choices)
+        return rhs & lhs
+
+
+class Or(Constraint):
+    """
+    Disjoins two constraints.
+    """
+
+    def __init__(self, lhs: Constraint, rhs: Constraint):
+        super().__init__()
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def __str__(self):
+        return f"Or({self.lhs}, {self.rhs})"
+
+    def __call__(self, schema: Schema, choices: torch.Tensor) -> torch.Tensor:
+        lhs = self.lhs(schema, choices)
+        rhs = self.rhs(schema, choices)
+        return rhs | lhs
+
+
+class Xor(Constraint):
+    """
+    Exclusive or among constraints. Equivalent to ``Not(Iff(lhs, rhs))``.
+    """
+
+    def __init__(self, lhs: Constraint, rhs: Constraint):
+        super().__init__()
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def __str__(self):
+        return f"Xor({self.lhs}, {self.rhs})"
+
+    def __call__(self, schema: Schema, choices: torch.Tensor) -> torch.Tensor:
+        lhs = self.lhs(schema, choices)
+        rhs = self.rhs(schema, choices)
+        return rhs ^ lhs
 
 
 class IfThen(Constraint):
--- test/test_constraints.py
+++ test/test_constraints.py
@@ -1,7 +1,22 @@
 from collections import OrderedDict
+from typing import List
 
 from pyroed.api import encode_design
-from pyroed.constraints import AllDifferent, Iff, IfThen, TakesValue
+from pyroed.constraints import (
+    AllDifferent,
+    And,
+    Iff,
+    IfThen,
+    Not,
+    Or,
+    TakesValue,
+    TakesValues,
+    Xor,
+)
+
+
+def stringify(bools: List[bool]) -> str:
+    return "".join("1" if x else "0" for x in bools)
 
 
 def test_immune_sequence():
@@ -43,3 +58,122 @@ def test_immune_sequence():
     assert actual[3] == [True, True, True, True, False, True, True]
     assert actual[4] == [True, True, True, True, True, False, True]
     assert actual[5] == [True, True, True, True, True, True, False]
+
+
+def test_takes_value():
+    SCHEMA = OrderedDict()
+    SCHEMA["foo"] = ["a", "b", "c", None]
+    SCHEMA["bar"] = ["a", "b", None]
+
+    CONSTRAINTS = [
+        TakesValue("foo", "a"),
+        TakesValue("foo", "b"),
+        TakesValue("foo", "c"),
+        TakesValue("foo", None),
+        TakesValue("bar", "a"),
+        TakesValue("bar", "b"),
+        TakesValue("bar", None),
+    ]
+
+    design = [
+        ["a", "a"],
+        ["a", "b"],
+        ["a", None],
+        ["b", "a"],
+        ["b", "b"],
+        ["b", None],
+        ["c", "a"],
+        ["c", "b"],
+        ["c", None],
+        [None, "a"],
+        [None, "b"],
+        [None, None],
+    ]
+
+    sequences = encode_design(SCHEMA, design)
+    actual = [c(SCHEMA, sequences).tolist() for c in CONSTRAINTS]
+    assert stringify(actual[0]) == "111000000000"
+    assert stringify(actual[1]) == "000111000000"
+    assert stringify(actual[2]) == "000000111000"
+    assert stringify(actual[3]) == "000000000111"
+    assert stringify(actual[4]) == "100100100100"
+    assert stringify(actual[5]) == "010010010010"
+    assert stringify(actual[6]) == "001001001001"
+
+
+def test_takes_values():
+    SCHEMA = OrderedDict()
+    SCHEMA["foo"] = ["a", "b", "c", None]
+    SCHEMA["bar"] = ["a", "b", None]
+
+    CONSTRAINTS = [
+        TakesValues("foo", "a"),
+        TakesValues("foo", "b", "c"),
+        TakesValues("foo", "a", None),
+        TakesValues("bar", "a", "b", None),
+        TakesValues("bar", "b"),
+        TakesValues("bar"),
+    ]
+
+    design = [
+        ["a", "a"],
+        ["a", "b"],
+        ["a", None],
+        ["b", "a"],
+        ["b", "b"],
+        ["b", None],
+        ["c", "a"],
+        ["c", "b"],
+        ["c", None],
+        [None, "a"],
+        [None, "b"],
+        [None, None],
+    ]
+
+    sequences = encode_design(SCHEMA, design)
+    actual = [c(SCHEMA, sequences).tolist() for c in CONSTRAINTS]
+    assert stringify(actual[0]) == "111000000000"
+    assert stringify(actual[1]) == "000111111000"
+    assert stringify(actual[2]) == "111000000111"
+    assert stringify(actual[3]) == "111111111111"
+    assert stringify(actual[4]) == "010010010010"
+    assert stringify(actual[5]) == "000000000000"
+
+
+def test_logic():
+    SCHEMA = OrderedDict()
+    SCHEMA["foo"] = ["a", None]
+    SCHEMA["bar"] = ["a", None]
+
+    foo = TakesValue("foo", "a")
+    bar = TakesValue("bar", "a")
+    CONSTRAINTS = [
+        foo,
+        bar,
+        Not(foo),
+        Not(bar),
+        And(foo, bar),
+        Or(foo, bar),
+        Xor(foo, bar),
+        Iff(foo, bar),
+        IfThen(foo, bar),
+    ]
+
+    design = [
+        ["a", "a"],
+        ["a", None],
+        [None, "a"],
+        [None, None],
+    ]
+
+    sequences = encode_design(SCHEMA, design)
+    actual = [c(SCHEMA, sequences).tolist() for c in CONSTRAINTS]
+    assert stringify(actual[0]) == "1100"
+    assert stringify(actual[1]) == "1010"
+    assert stringify(actual[2]) == "0011"
+    assert stringify(actual[3]) == "0101"
+    assert stringify(actual[4]) == "1000"
+    assert stringify(actual[5]) == "1110"
+    assert stringify(actual[6]) == "0110"
+    assert stringify(actual[7]) == "1001"
+    assert stringify(actual[8]) == "1011"
