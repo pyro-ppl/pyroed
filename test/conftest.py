ee079df36fb6aac2e4196179994fbf40061a781d
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2022-03-24 16:28:02 -0400

    Fix random see of tests

---
+++ test/conftest.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pyro
+
+
+def pytest_runtest_setup(item):
+    np.random.seed(20220324)
+    pyro.set_rng_seed(20220324)
+    pyro.enable_validation(True)
+    pyro.clear_param_store()
